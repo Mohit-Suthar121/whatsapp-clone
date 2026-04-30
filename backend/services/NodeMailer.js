f8de4d75db92b296b6e9a0b44711f50d3822bee7
MohitSuthar <mohitsuthar5006@gmail.com>
2026-04-30 11:27:26 +0530

    Changed the nodemailer port

--- backend/services/NodeMailer.js
+++ backend/services/NodeMailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-  port: 587,
+  port: 2525,
   secure: false,
     auth: {
         pass: process.env.BREVO_SMTP_KEY,
